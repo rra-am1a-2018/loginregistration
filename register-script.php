aa40d6101ee7d2a011be71fa2ec90585010ba5ea
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-02-18 09:54:29 +0100

    check email exits

--- register-script.php
+++ register-script.php
@@ -9,24 +9,26 @@
   // Stuur de query af op de database
   $result = mysqli_query($conn, $sql);
 
-  echo mysqli_num_rows($conn, $result); exit();
-
-  $sql = "INSERT INTO `register` (`id`,
+  if (mysqli_num_rows($result)) {
+    // Email is al in gebruik
+    echo '<div class="alert alert-info" role="alert">Het door u ingevoerde e-mailadres is al in gebruik, kies een ander e-mailadres</div>';
+    header("Refresh: 4; url=./index.php?content=register");
+  } else {
+    $sql = "INSERT INTO `register` (`id`,
                                   `email`,
                                   `password`)
                           VALUES (NULL,
                                   '$email',
                                   'geheim')";
   
-  $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
 
-  if ($result) {
-    echo '<div class="alert alert-success" role="alert">
-    Uw emailadres is verwerkt
-  </div>';
-    header("refresh:2; url=./index.php?content=register");
-  } else {
-    echo 'Er is iets verschrikkelijks niet in orde met deze code. Zoek een andere programmeur!';
+    if ($result) {
+      echo '<div class="alert alert-success" role="alert">Uw emailadres is verwerkt</div>';
+      header("Refresh: 4; url=./index.php?content=register");
+    } else {
+      echo 'Uw gegevens zijn niet verwerkt probeer het nog een keer op een ander moment, dat is alles wat we weten';
+      header("Refresh: 4; url=./index.php?content=register");
+    }
   }
-
 ?>
