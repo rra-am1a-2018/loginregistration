d4bb2ff73bb029e686eaf43c884c99947b5e6c96
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-11 09:16:33 +0100

    password

--- choosepassword-script.php
+++ choosepassword-script.php
@@ -7,6 +7,20 @@
   $id =  sanitize($_POST["id"]);
 
   $sql = "UPDATE `register` 
-          SET `password` = $password
+          SET `password` = '$password'
           WHERE `id` = $id";
+
+  $result = mysqli_query($conn, $sql);
+
+  if ( $result ) {
+    echo '<div class="alert alert-success" role="alert">U password is ingesteld</div>';
+    header("Refresh: 4; url=./index.php?content=homepage");
+  } else {
+    echo '<div class="alert alert-danger" role="alert">Uw wachtwoord is niet ingesteld. Probeer het nogmaals...</div>';
+      header("Refresh: 4; url=./index.php?content=choosepassword&id=$id");
+  }
+
+
+
+  
 ?>
