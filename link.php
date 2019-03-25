4323d352458ab8cd749ffc197283becd7968c5e4
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-03-25 10:36:40 +0100

    userrole dependent links

--- link.php
+++ link.php
@@ -10,6 +10,34 @@
       <?php
         // Deze links worden aangeboden als we zijn ingelogd.
         if (isset($_SESSION["id"])) {
+          switch($_SESSION["userrole"]) {
+            case 'administrator':
+              echo '<li class="nav-item">
+                      <a class="nav-link" href="http://www.loginregistration.am1a.org/index.php?content=administratorhome">adminhome</a>
+                    </li>';
+            break;
+            case 'root':
+              echo '<li class="nav-item">
+                      <a class="nav-link" href="http://www.loginregistration.am1a.org/index.php?content=roothome">roothome</a>
+                    </li>';
+            break;
+            case 'moderator':
+              echo '<li class="nav-item">
+                      <a class="nav-link" href="http://www.loginregistration.am1a.org/index.php?content=moderatorhome">moderatorhome</a>
+                    </li>';
+            break;
+            case 'customer':
+              echo '<li class="nav-item">
+                      <a class="nav-link" href="http://www.loginregistration.am1a.org/index.php?content=customerhome">customerhome</a>
+                    </li>';
+            break;
+            default:
+              header("Location: ./index.php?content=logout");
+            break;
+          }
+
+
+
           echo '<li class="nav-item active">
                   <a class="nav-link" href="http://www.loginregistration.am1a.org/index.php?content=homepage">home<span class="sr-only">(current)</span></a>
                 </li>';
